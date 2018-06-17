978c7e5866e383720503b4cee86d2fb637e9e164
Angel Petrov <a.demerdzhiev@gmail.com>
2018-06-17 15:51:35 +0300

    Add files via upload

---
+++ DefiningClasses/ex1/Main.java
@@ -0,0 +1,12 @@
+package ex1;
+
+import java.lang.reflect.Field;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Class person = Person.class;
+        Field[] fields = person.getDeclaredFields();
+        System.out.println(fields.length);
+    }
+ }
---
+++ DefiningClasses/ex1/Person.java
@@ -0,0 +1,6 @@
+package ex1;
+
+public class Person {
+    private String name;
+    private int age;
+}
